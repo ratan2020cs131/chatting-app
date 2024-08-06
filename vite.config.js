33398987cc276551a8115698e63242504f42cfa1
Ratan Deep Singh <91245947+ratan2020cs131@users.noreply.github.com>
2024-08-06 23:04:59 +0530

    vite config done

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,30 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    port: 3001,
+    https: true,
+    fs: {
+      cachedChecks: false,
+    },
+  },
+  resolve: {
+    alias: {
+      assets: "/src/assets",
+      components: "/src/components",
+      constants: "/src/constants",
+      hooks: "/src/hooks",
+      routes: "/src/routes",
+      screen: "/src/screens",
+      services: "/src/services",
+      utils: "/src/utils",
+    },
+  },
+  preview: {
+    port: 3001,
+    strictPort: true,
+  },
+});
