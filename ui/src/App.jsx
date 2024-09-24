57db436ceb50241f37a5b1c5b9d6d0a010209a4d
Ratan Deep Singh <91245947+ratan2020cs131@users.noreply.github.com>
2024-09-24 15:06:09 +0530

    done: text area component

--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -1,6 +1,7 @@
 import { SearchIcon } from "assets/svg";
 import "./index.css";
 import TextInput from "components/TextInput";
+import TextArea from "components/TextArea";
 
 const App = () => {
   return (
@@ -8,6 +9,10 @@ const App = () => {
       <TextInput variant="error" title="Error" placeholder="This is error input" required />
       <TextInput variant="success" title="Success" placeholder="This is success input" required />
       <TextInput icon={SearchIcon} title="Default" placeholder="This is default input" />
+
+      <TextArea variant="error" title="Error" placeholder="This is error input" required />
+      <TextArea variant="success" title="Success" placeholder="This is success input" required />
+      <TextArea title="Default" placeholder="This is default input" />
     </div>
   );
 };
