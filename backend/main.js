f17311e77c0cdf967cd9eced2557da18e7a42600
Ratan Deep Singh <91245947+ratan2020cs131@users.noreply.github.com>
2024-10-10 14:21:05 +0530

    init: socket-service

--- backend/main.js
+++ backend/main.js
@@ -1,18 +1,29 @@
 import express from "express";
+import { createServer } from "http";
 import { logger, logRequests } from "./utils/logger.util.js";
-import { Connectmongo } from "./database/connection.js";
+import { ConnectMongo } from "./database/connection.js";
 import dotenv from "dotenv";
-import cors from "cors";
+import indexRoute from "./routes/index.js";
+import { Server } from "socket.io";
+import { initSocket } from "./socket/index.js";
+import { socketEventEmitterMiddleWare } from "./socket/socket.emit.js";
 
 dotenv.config({ path: "./.env" });
-Connectmongo();
-const app = express();
 
+ConnectMongo();
+
+const app = express();
+const server = createServer(app);
+const Socket = new Server(server);
+app.set("socket", Socket);
+initSocket(Socket);
+
+app.use(socketEventEmitterMiddleWare);
 app.use(logRequests);
 
 app.use(express.json());
-app.use(IndexRouter);
+app.use(indexRoute);
 
-app.listen(5001, () => {
-  logger.info(`service running on port ${5001}`);
+server.listen(process.env.PORT, () => {
+  logger.info(`service running on port ${process.env.PORT}`);
 });
