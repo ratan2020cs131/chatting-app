fba2c16fbe2e8d3c00150d269906bdfca82dd8c6
Ratan Deep Singh <91245947+ratan2020cs131@users.noreply.github.com>
2024-11-21 02:47:11 +0530

    eslint setup

--- backend/eslint.config.js
+++ backend/eslint.config.js
@@ -1,17 +1,36 @@
 import js from "@eslint/js";
+import eslintPluginPrettier from "eslint-plugin-prettier";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
+import globals from "globals";
 
 export default [
-  js.configs.recommended,
   {
+    ...js.configs.recommended, // Use recommended configuration from @eslint/js
+    languageOptions: {
+      globals: globals.node, // Add Node.js global variables
+    },
+  },
+  {
+    plugins: {
+      prettier: eslintPluginPrettier, // Add Prettier plugin
+      "simple-import-sort": simpleImportSort, // Add Simple Import Sort plugin
+    },
     rules: {
       "no-unused-vars": "error",
       "no-undef": "error",
-      camelcase: [
+      camelcase: ["error", { properties: "always" }],
+      "prettier/prettier": "error",
+      "simple-import-sort/imports": [
         "error",
         {
-          properties: "always", // Enforces camelCase for object properties as well
+          groups: [
+            ["^@?\\w"], // Third-party packages
+            ["^@/(.*)$"], // Absolute imports
+            ["^[./]"], // Relative imports
+          ],
         },
       ],
+      "no-console": "error",
     },
   },
 ];
