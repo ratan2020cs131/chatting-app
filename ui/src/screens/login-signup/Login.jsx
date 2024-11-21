3398f64185ad1df1d3f4fb3b6559c89c68355c11
Ratan Deep Singh <91245947+ratan2020cs131@users.noreply.github.com>
2024-11-21 23:51:34 +0530

    fixed: sign in pages made responsive

--- ui/src/screens/login-signup/Login.jsx
+++ ui/src/screens/login-signup/Login.jsx
@@ -6,29 +6,57 @@ import Link from "components/Link";
 import TextInput from "components/TextInput";
 import {
   TypoExtraLargeSemiBold,
-  TypoLargeSemiBold,
   TypoMediumSemiBold,
 } from "components/TypoGraphy";
 import React from "react";
+import { useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import AppContainer from "screens/layouts/AppContainer";
 const Login = () => {
   const navigate = useNavigate();
+  const touchStartY = useRef(0);
+  const [isVisible, setIsVisible] = useState(true);
+
+  const handleTouchStart = (e) => {
+    touchStartY.current = e.touches[0].clientY;
+  };
+
+  const handleTouchEnd = (e) => {
+    const touchEndY = e.changedTouches[0].clientX;
+    if (touchStartY.current - touchEndY > 20) setIsVisible(false);
+    if (touchEndY - touchStartY.current > 20) setIsVisible(true);
+  };
+
   return (
     <AppContainer>
-      <div className="grid grid-cols-2 h-full">
-        <div className="bg-primary-blue flex flex-col items-center justify-center">
+      <div
+        className={`grid grid-cols-1 md:grid-cols-2 h-full transition-transform duration-500 ${
+          isVisible
+            ? "transform translate-y-0"
+            : "transform translate-y-[-100%] md:translate-y-0"
+        }`}
+        onTouchStart={handleTouchStart}
+        onTouchEnd={handleTouchEnd}
+      >
+        <div className="bg-primary-blue h-screen flex flex-col items-center justify-center overflow-hidden">
           <IconWrapper Icon={LoginSvg} size="xxlarge" />
         </div>
-        <div className="flex flex-col items-center justify-center gap-16">
+        <div className="flex flex-col items-center justify-center gap-16 px-4 py-8">
           <span className="flex flex-col gap-2 items-center">
             <TypoExtraLargeSemiBold>Welcome!</TypoExtraLargeSemiBold>
             <TypoMediumSemiBold>Login to your acoount</TypoMediumSemiBold>
           </span>
-          <div className="w-[24rem] flex flex-col items-center justify-center gap-4">
-            <TextInput required title="Email" placeholder="Enter your email" icon={MailIcon} />
+          <div className="w-full md:w-[24rem] flex flex-col items-center justify-center gap-4">
+            <TextInput
+              fullWidth
+              required
+              title="Email"
+              placeholder="Enter your email"
+              icon={MailIcon}
+            />
             <TextInput
               required
+              fullWidth
               secret
               title="Password"
               placeholder="Enter your password"
--- ui/src/screens/login-signup/Signup.jsx
+++ ui/src/screens/login-signup/Signup.jsx
@@ -1,32 +1,68 @@
-import { LockIcon, MailIcon, RightArrowIcon, RegisterSvg, UserIcon } from "assets/svg";
+import {
+  LockIcon,
+  MailIcon,
+  RightArrowIcon,
+  RegisterSvg,
+  UserIcon,
+} from "assets/svg";
 import CallIcon from "assets/svg/CallIcon";
 import Button from "components/Button";
 import SelectDropdown from "components/SelectDropdown";
 import TextInput from "components/TextInput";
-import { TypoExtraLargeSemiBold, TypoMediumSemiBold } from "components/TypoGraphy";
+import {
+  TypoExtraLargeSemiBold,
+  TypoMediumSemiBold,
+} from "components/TypoGraphy";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import AppContainer from "screens/layouts/AppContainer";
 import { countryCode } from "screens/login-signup/constants";
 import IconWrapper from "components/IconWrapper";
+import { useRef } from "react";
 
 const Signup = () => {
   const navigate = useNavigate();
+  const touchStartY = useRef(0);
   const [phone, setPhone] = useState();
+  const [isVisible, setIsVisible] = useState(true);
+
+  const handleTouchStart = (e) => {
+    touchStartY.current = e.touches[0].clientY;
+  };
+
+  const handleTouchEnd = (e) => {
+    const touchEndY = e.changedTouches[0].clientX;
+    if (touchStartY.current - touchEndY > 20) {
+      console.log("scroll up");
+      setIsVisible(false);
+    }
+    if (touchEndY - touchStartY.current > 20) setIsVisible(true);
+  };
 
   return (
     <AppContainer>
-      <div className="grid grid-cols-2 h-full">
-        <div className="bg-primary-blue flex flex-col items-center justify-center">
+      <div
+        className={`grid grid-cols-1 md:grid-cols-2 h-full transition-transform duration-500 ${
+          isVisible
+            ? "transform translate-y-0"
+            : "transform translate-y-[-100%] md:translate-y-0"
+        }`}
+        onTouchStart={handleTouchStart}
+        onTouchEnd={handleTouchEnd}
+      >
+        <div className="bg-primary-blue flex flex-col items-center justify-center h-screen overflow-hidden">
           <IconWrapper size="xxlarge" Icon={RegisterSvg} />
         </div>
-        <div className="flex flex-col items-center justify-center gap-16">
+        <div className="flex flex-col items-center justify-center gap-16 px-4 py-8">
           <span className="flex flex-col gap-2 items-center">
             <TypoExtraLargeSemiBold>Welcome!</TypoExtraLargeSemiBold>
-            <TypoMediumSemiBold>Create a new account to explore</TypoMediumSemiBold>
+            <TypoMediumSemiBold>
+              Create a new account to explore
+            </TypoMediumSemiBold>
           </span>
-          <div className="w-[24rem] flex flex-col items-center justify-center gap-4">
+          <div className="w-full md:w-[24rem] flex flex-col items-center justify-center gap-4">
             <TextInput
+              fullWidth
               title="Full name"
               placeholder="Enter your full name"
               icon={UserIcon}
@@ -49,8 +85,20 @@ const Signup = () => {
                 onChange={(value) => setPhone(value)}
               />
             </span>
-            <TextInput type="email" title="Email" placeholder="Enter your email" icon={MailIcon} />
-            <TextInput secret title="Password" placeholder="Enter your password" icon={LockIcon} />
+            <TextInput
+              fullWidth
+              type="email"
+              title="Email"
+              placeholder="Enter your email"
+              icon={MailIcon}
+            />
+            <TextInput
+              fullWidth
+              secret
+              title="Password"
+              placeholder="Enter your password"
+              icon={LockIcon}
+            />
             <Button fullWidth icon={RightArrowIcon} title="Signup" />
             <Button
               fullWidth
