82cbe783a5e41702bcb7b6646a81d0ec17effd39
Ratan Deep Singh <91245947+ratan2020cs131@users.noreply.github.com>
2024-12-04 23:45:33 +0530

    be: re structured (#11)

--- ui/src/assets/svg/DobleChevronUpIcon.jsx
+++ ui/src/assets/svg/DobleChevronUpIcon.jsx
@@ -2,7 +2,7 @@ import React from "react";
 const DobleChevronUpIcon = ({ height = 1, width = 1, fill = "#0F0F0F" }) => {
   return (
     <svg
-      viewBox="0 0 24 24"
+      viewBox="-0.5 0 15 15"
       height={`${height}rem`}
       width={`${width}rem`}
       xmlns="http://www.w3.org/2000/svg"
